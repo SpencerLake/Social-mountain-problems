cdb64ea1ee9d47f6f31e2c51ef06758b9ed9e17f
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-10-26 12:22:28 -0400

    finished problems

--- src/components/App.js
+++ src/components/App.js
@@ -29,16 +29,24 @@ class App extends Component {
       })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, { text })
+      .then( results => { this.setState({ posts: results.data });
+    })  ;
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`)
+      .then( results => {
+        this.setState({ posts: results.data });
+      });
   }
 
-  createPost() {
-
+  createPost( text ) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, { text })
+      .then(results => {
+        this.setState({ posts: results.data });
+      });
   }
 
   render() {
@@ -50,11 +58,16 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={ this.createPost } />
           
           {
             posts.map( post => (
-              <Post key={ post.id } text={post.text} date={post.date} />
+              <Post key={ post.id } 
+                    text={post.text} 
+                    date={post.date}
+                    id={ post.id }
+                    updatePostFn={ this.updatePost }
+                    deletePostFn={ this.deletePost } />
             ))
           }
 
